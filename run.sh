fae6d84ccf34c1e7812cd151c18524ac727b8932
Christian Peth <sspectre99@gmail.com>
2020-05-13 11:01:31 -0700

    dockerhub image

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ git clone https://github.com/blastmotion/fastai notebooks/fastai
 mkdir notebooks/fastai/courses/ml1/data
 mkdir notebooks/fastai/courses/ml1/data/bulldozers
 ln -s notebooks/fastai/data/bulldozers notebooks/fastai/courses/ml1/data/bulldozers
-docker run -p 8888:8888 --name blast-ml -v $(pwd)/notebooks:/opt/notebooks blastai
+docker run -p 8888:8888 --name blast-ml -v $(pwd)/notebooks:/opt/notebooks blastmotion/blastai:latest
