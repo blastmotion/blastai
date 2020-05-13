fd12c2282dcab2d0d7c8931bc92edc0867bd116e
Christian Peth <sspectre99@gmail.com>
2020-05-12 21:27:05 -0700

    adding data symlink

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 mkdir notebooks
 git clone https://github.com/blastmotion/fastai notebooks/fastai
-docker run -it -p 8888:8888 --name blast-ml -v $(pwd)/notebooks:/opt/notebooks blastai
+ln notebooks/fastai/data/bulldozers notebook/fastai/courses/ml1/data/bulldozers
+docker run -p 8888:8888 --name blast-ml -v $(pwd)/notebooks:/opt/notebooks blastai
