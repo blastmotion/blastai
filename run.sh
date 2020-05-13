1c9387d618c986176d3828179676a2fa2bace2f7
Christian Peth <sspectre99@gmail.com>
2020-05-12 21:37:35 -0700

    made sym soft

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 mkdir notebooks
 git clone https://github.com/blastmotion/fastai notebooks/fastai
-ln notebooks/fastai/data/bulldozers notebook/fastai/courses/ml1/data/bulldozers
+ln -s notebooks/fastai/data/bulldozers notebook/fastai/courses/ml1/data/bulldozers
 docker run -p 8888:8888 --name blast-ml -v $(pwd)/notebooks:/opt/notebooks blastai
