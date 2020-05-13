f4ec406c73b40f15d986c6ff7b427d0c0d668d0e
Christian Peth <sspectre99@gmail.com>
2020-05-12 21:44:40 -0700

    symlink fixes

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 
 mkdir notebooks
 git clone https://github.com/blastmotion/fastai notebooks/fastai
-ln -s notebooks/fastai/data/bulldozers notebook/fastai/courses/ml1/data/bulldozers
+mkdir notebooks/fastai/courses/ml1/data
+mkdir notebooks/fastai/courses/ml1/data/bulldozers
+ln -s notebooks/fastai/data/bulldozers notebooks/fastai/courses/ml1/data/bulldozers
 docker run -p 8888:8888 --name blast-ml -v $(pwd)/notebooks:/opt/notebooks blastai
